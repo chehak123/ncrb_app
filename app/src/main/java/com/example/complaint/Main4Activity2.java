69de2e4b51e7d7129a6db6b44d9a69fee7289b77
chehak agrawal <53402167+chehak123@users.noreply.github.com>
2020-01-20 20:57:45 +0530

    police login page

---
+++ app/src/main/java/com/example/complaint/Main4Activity2.java
@@ -0,0 +1,14 @@
+package com.example.complaint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main4Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
